dbcd6f33e221228c7ff8ea11226940cb144828d0
Deviletha <devicathira1991@gmail.com>
2023-03-29 10:05:14 +0530

    Initial commit

--- lib/Utility/todo_tile.dart
+++ lib/Utility/todo_tile.dart
@@ -26,7 +26,7 @@ class ToDoTile extends StatelessWidget {
             SlidableAction(
               onPressed: deleteFunction,
               icon: Icons.delete,
-              backgroundColor: Colors.red.shade300,
+              backgroundColor: Colors.redAccent.shade100,
               borderRadius: BorderRadius.circular(12),
             )
           ],
@@ -34,7 +34,7 @@ class ToDoTile extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(24),
           decoration: BoxDecoration(
-            color: Colors.yellow,
+            color: Colors.teal,
             borderRadius: BorderRadius.circular(12),
           ),
           child: Row(
